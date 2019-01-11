540d17a080b9fc0364801fe46837090185d9499a
Hayden Shively <haydenshively@gmail.com>
2019-01-11 01:04:06 -0800

    Optimized symmetry finder in castle.js

--- javascript/castle.js
+++ javascript/castle.js
@@ -4,6 +4,20 @@ import {find_path} from './astar.js';
 export class Castle {
   constructor() {
     this.step = 0;
+
+    this.map_rows = null;
+    this.map_cols = null;
+    this.symmetry_style = null;
+
+    this.initialized = false;
+  }
+
+  initialize_with(puppet) {
+    this.map_rows = puppet.map.length;
+    this.map_cols = puppet.map[0].length;
+    this.symmetry_style = this.find_symmetry_style(puppet.map);
+
+    this.initialized = true;
   }
 
   /*
@@ -17,30 +31,31 @@ export class Castle {
   return puppet.castleTalk(value);
   */
   get_action_for(puppet) {
+    if (!this.initialized) {this.initialize_with(puppet);}
+
     if (this.step == 0) {
-      //puppet.log("Building a crusader at " + (puppet.me.x+1) + ", " + (puppet.me.y+1));
       return puppet.buildUnit(SPECS.CRUSADER, 1, 1);
-    }else {
-      return // puppet.log("Castle health: " + puppet.me.health);
     }
   }
 
-  find_symmetry(map) {
-    let rows = map.length;
-    let cols = map[0].length;
+  find_symmetry_style(map) {
     var left_right = true;
     var top_bottom = true;
     loop1:
-    for (var i = 0; i < rows/2; i++) {
+    for (var i = 0; i < this.map_rows/2; i++) {
       let top = map[i];
-      let bottom = map[rows - 1 - i];
+      let bottom = map[this.map_rows - 1 - i];
       loop2:
-      for (var j = 0; j < cols/2; j++) {
+      for (var j = 0; j < this.map_cols/2; j++) {
         if (top[j] != bottom[j]) {top_bottom = false; break loop1;}
-        else if (top[j] != top[cols - 1 - j]) {left_right = false; break loop1;}
+        else if (top[j] != top[this.map_cols - 1 - j]) {left_right = false; break loop1;}
       }
     }
 
     return [left_right, top_bottom];
   }
+
+  // find_partner_for(coordinates, symmetry) {
+  //
+  // }
 }
