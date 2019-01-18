020e8112b5168670a4b2555f5de69270637ade59
Hayden Shively <haydenshively@gmail.com>
2019-01-18 01:56:57 -0800

    Pilgrim now builds church

--- javascript/pilgrim.js
+++ javascript/pilgrim.js
@@ -2,25 +2,17 @@ import {SPECS} from 'battlecode';
 import {CommonSource} from './common.js';
 import {find_path} from './astar.js';
 
-// class Castle {
-//   constructor(id = null, x = null, y = null, id_bool = null) {
-//     this.id = id;
-//     this.x = x;
-//     this.y = y;
-//     this.id_bool = id_bool;
-//   }
-// }
-
 export class PilgrimSource extends CommonSource{
   constructor() {
     super();
     // Variables to set once (as soon as we receive a puppet instance)
-    this.meta_turn = -1;
     this.parent = null;
     this.parent_signal = -1;
     this.friend_count = 0;
-    // Are all the above set?
-    this.initialized = false;
+
+    this.path = [];
+
+    this.has_built_church = false;
   }
 
   /*
@@ -35,22 +27,34 @@ export class PilgrimSource extends CommonSource{
   return puppet.castleTalk(value);
   */
   get_action_for(puppet) {
-    if (!this.initialized) {this.initialize_with(puppet);}
-    this.meta_turn++;
+    if (puppet.me.turn == 1) {
+      this.initialize_with(puppet);
+      this.update_path(puppet, puppet.map, this.most_crucial_resource_map(puppet));
+    }
 
-    if (this.parent_signal > -1) {
-      //TODO
+    if (this.path.length > 1) {
+      let dx = this.path[1][0] - puppet.me.x;
+      let dy = this.path[1][1] - puppet.me.y;
+      this.path.shift();
+      return puppet.move(dx, dy);
     }else {
-      let resource_map = (puppet.karbonite <= puppet.fuel ? puppet.karbonite_map : puppet.fuel_map);// TODO prob don't want to switch after on tile
-      let target = super.find_nearest_resource([puppet.me.x, puppet.me.y], resource_map);
-      let path = find_path([puppet.me.x, puppet.me.y], target, puppet.map, SPECS.PILGRIM);
-
-      if (path.length > 1) {
-        let dx = path[1][0] - puppet.me.x;
-        let dy = path[1][1] - puppet.me.y;
-        return puppet.move(dx, dy);
+      if (!this.has_built_church && this.can_build_church(puppet)) {
+        return puppet.buildUnit(SPECS.CHURCH, -1, 1);
       }
     }
+  }
+
+  can_build_church(puppet) {
+    let spec = SPECS.UNITS[SPECS.CHURCH];
+    return (puppet.karbonite >= spec.CONSTRUCTION_KARBONITE) && (puppet.fuel >= spec.CONSTRUCTION_FUEL);
+  }
+
+  most_crucial_resource_map(puppet) {return (puppet.karbonite <= puppet.fuel ? puppet.karbonite_map : puppet.fuel_map);}
+
+  // TODO read data from signal instead of constant [15, 15]
+  update_path(puppet, terrain_map, resource_map) {
+    let target = (this.parent_signal > -1 ? [15, 15] : super.find_nearest_resource([puppet.me.x, puppet.me.y], resource_map));
+    this.path = find_path([puppet.me.x, puppet.me.y], target, terrain_map, SPECS.PILGRIM);
   }
 
   initialize_with(puppet) {
@@ -70,9 +74,7 @@ export class PilgrimSource extends CommonSource{
         inst.friend_count++;
       }
     }
-    function completion(robot, inst) {inst.meta_turn += inst.parent.turn;}
+    function completion(robot, inst) {}
     super.process_visible_robots_using(puppet, handle_enemy, handle_friendly, completion);
-
-    this.initialized = true;
   }
 }
